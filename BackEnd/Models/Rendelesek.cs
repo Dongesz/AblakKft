5716ae87d1a319d94d5173984c9db49f68a9507f
Dongesz <majormate12@gmail.com>
2025-09-28 00:46:40 +0200

    feat(BackEnd): Implement Modles
    - Orders.cs: Model class for orders table
    - Users.cs: Model class for users table

---
+++ BackEnd/Models/Rendelesek.cs
@@ -0,0 +1,15 @@
+﻿namespace AblakKft.Models
+{
+    public class Rendelesek
+    {
+        private int _id {  get; set; }
+        private int _user_id {  get; set; }
+        private int _product_id {  get; set; }
+        private int _quantity {  get; set; }
+        private string _shipping_adress {  get; set; }
+        private string _status { get; set; }
+        private DateTime _order_date { get; set; }
+        
+        
+    }
+}
